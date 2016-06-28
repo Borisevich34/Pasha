4c549d0aca1fe628adce95b52e2c9924c3c89724
Pavel Borisevich <borisevich34pavel@gmail.com>
2016-06-28 11:22:35 +0300

    TableView added

--- Pasha/Pasha/ViewController.swift
+++ Pasha/Pasha/ViewController.swift
@@ -8,11 +8,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+import Alamofire
 
+class ViewController: UITableViewController {
+
+    var channels = ["channel1", "channel2", "channel3"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        Alamofire.request(.GET, "http://feeds.bbci.co.uk/news/world/rss.xml", parameters: nil)
+            .response { request, response, data, error in
+                if (error != nil) {
+                    print("Error!")
+                } else {
+                    print(data!)
+                }
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +32,18 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return channels.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = self.tableView.dequeueReusableCellWithIdentifier("channel")! as UITableViewCell
+        
+        cell.textLabel?.text = channels[indexPath.row]
+        
+        return cell
+    }
+    
+    
 
 }
-
